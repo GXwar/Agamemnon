e6906079b3624f1b199fe2f2358e844afdd7860d
GXwar <richardxulh@gmail.com>
2018-09-06 21:25:08 -0400

    encapsulate context

---
+++ test/original.js
@@ -0,0 +1,34 @@
+const Koa = require('koa');
+
+let app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  console.log(ctx);
+  console.log(ctx.request);
+  console.log(ctx.response);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(ms)
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
